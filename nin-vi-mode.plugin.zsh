f831e3b34cf04dfb32a8a9f6b1c0076bd70b31b8
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-01-06 13:22:06 -0200

    improving comments

--- nin-vi-mode.plugin.zsh
+++ nin-vi-mode.plugin.zsh
@@ -78,6 +78,19 @@ bindkey "^H" backward-delete-char
 bindkey "^?" backward-delete-char
 
 #numeric keypad return (enter)
+# see https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=685511
+# [snip]
+# if (( ${+terminfo[kent]}  )); then
+#     kent="${terminfo[kent]}"
+# else
+#     kent=$(TERM=xterm echoti kent 2>/dev/null)
+#     # If there is nothing in `$kent' yet, use "ESC O M", which is
+#     # what xterm and urxvt use.
+#     [[ -z "$kent"  ]] && kent=$'\eOM'
+# fi
+# bindkey "$kent" accept-line
+# unset kent
+# [snap]
 bindkey "${terminfo[kent]}" accept-line
 
 # if mode indicator wasn't setup by theme, define default
