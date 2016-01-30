fa51a10f37341f3e7b81822dec2e1adfe7fda580
ninrod <filipe.silva@gmail.com>
2016-01-30 09:42:50 -0200

    - removing right prompt - removing superfluous code - adding helpful comments - organizing sections

--- nin-vi-mode.plugin.zsh
+++ nin-vi-mode.plugin.zsh
@@ -1,19 +1,8 @@
-# Ensures that $terminfo values are valid and updates editor information when
-# the keymap changes.
+# no delays
+# export KEYTIMEOUT=1
 
-# configurações do modo vi do zsh
-export KEYTIMEOUT=1
-
-function zle-keymap-select zle-line-init zle-line-finish {
-  # The terminal must be in application mode when ZLE is active for $terminfo
-  # values to be valid.
-  if (( ${+terminfo[smkx]} )); then
-    printf '%s' ${terminfo[smkx]}
-  fi
-  if (( ${+terminfo[rmkx]} )); then
-    printf '%s' ${terminfo[rmkx]}
-  fi
-
+# Updates editor information when the keymap changes.
+function zle-keymap-select() {
   zle reset-prompt
   zle -R
 }
@@ -23,21 +12,20 @@ TRAPWINCH() {
   zle && { zle reset-prompt; zle -R }
 }
 
-zle -N zle-line-init
-zle -N zle-line-finish
 zle -N zle-keymap-select
-zle -N edit-command-line
 
 bindkey -v
 
-# allow vim to edit the command line (standard behaviour)
+# edit line with vim
 autoload -Uz edit-command-line
-bindkey -M vicmd 's' edit-command-line
+bindkey -M vicmd 'K' edit-command-line
 
-bindkey -M vicmd 'k' vi-up-line-or-history
-bindkey -M vicmd 'j' vi-down-line-or-history
+# since zsh 5.0.8, text objects were introduced. Let's use some of them.
+# see here for more info: http://www.zsh.org/mla/workers/2015/msg01017.html
+# and here: https://github.com/zsh-users/zsh/commit/d257f0143e69c3724466c4c92f59538d2f3fffd1
 
-# isso aqui é para que o `ci(` text object funcione no zle (somente a partir da versão 5.0.8 do zsh)
+# using select-bracketed as intructed on: https://github.com/zsh-users/zsh/blob/master/Functions/Zle/select-bracketed#L6
+# same as vim c+motion (change inside/around text-object).
 autoload -U select-bracketed
 zle -N select-bracketed
 for m in visual viopp; do
@@ -46,7 +34,8 @@ for m in visual viopp; do
   done
 done
 
-# esse código faz com que o `vi"` funcione
+# using select-quoted as instructed on: https://github.com/zsh-users/zsh/blob/master/Functions/Zle/select-quoted#L6
+# expands c+motion (change inside/around + text-object) to quotes.
 autoload -U select-quoted
 zle -N select-quoted
 for m in visual viopp; do
@@ -55,16 +44,7 @@ for m in visual viopp; do
   done
 done
 
-autoload -Uz surround
-zle -N delete-surround surround
-zle -N add-surround surround
-zle -N change-surround surround
-bindkey -a cs change-surround
-bindkey -a ds delete-surround
-bindkey -a ys add-surround
-bindkey -M visual S add-surround
-
-### fix de escape codes do zsh
+############# zsh escape code fixes #############
 
 # home key
 bindkey "^[[1~" beginning-of-line
@@ -72,37 +52,12 @@ bindkey "^[[1~" beginning-of-line
 # end key
 bindkey "^[[4~" end-of-line
 
-#delete key and backspace
+#delete key
 bindkey "^[[3~" delete-char
+
+# backspace key
 bindkey "^H" backward-delete-char
 bindkey "^?" backward-delete-char
 
 #numeric keypad return (enter)
-# see https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=685511
-# [snip]
-# if (( ${+terminfo[kent]}  )); then
-#     kent="${terminfo[kent]}"
-# else
-#     kent=$(TERM=xterm echoti kent 2>/dev/null)
-#     # If there is nothing in `$kent' yet, use "ESC O M", which is
-#     # what xterm and urxvt use.
-#     [[ -z "$kent"  ]] && kent=$'\eOM'
-# fi
-# bindkey "$kent" accept-line
-# unset kent
-# [snap]
 bindkey "${terminfo[kent]}" accept-line
-
-# if mode indicator wasn't setup by theme, define default
-if [[ "$MODE_INDICATOR" == "" ]]; then
-  MODE_INDICATOR="%{$fg_bold[blue]%}-- NORMAL --%{$reset_color%}"
-fi
-
-function vi_mode_prompt_info() {
-  echo "${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
-}
-
-# define right prompt, if it wasn't defined by a theme
-if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
-  RPS1='$(vi_mode_prompt_info)'
-fi
